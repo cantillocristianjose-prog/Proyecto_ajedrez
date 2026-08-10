2f16df3e823607491193c779a998d6e48b4b2b72
Littleroky <cantillocristianjose@gmail.com>
2026-08-10 08:40:46 -0500

    Actualizar links.py

--- link_bio/views/links/links.py
+++ link_bio/views/links/links.py
@@ -3,10 +3,10 @@ import views.constants as constants
 from styles.styles import Size,Spacing
 from components.link_button import link_button
 from components.title import title
-
+#se cambio el titulo comunidad por redes sociales y se agrego un nuevo titulo
 def links() -> rx.Component:
     return rx.vstack(
-        title("Comunidad"),
+        title("Redes sociales"),
         link_button("Instragram",
                     "Sígueme en Instagram",
                     constants.INSTAGRAM_URL),
@@ -16,8 +16,12 @@ def links() -> rx.Component:
         link_button("Twitch",
                     "Directos los sabados y festivos",
                     constants.TWITCH_URL),
+        title("Canales"),
         link_button("Youtube",
-                    "Videos largo en duracion ¡con el doble de diversion!",
+                    "Videos para pasar el rato",
                     constants.YOUTUBE_URL),
         width="100%"
     )
+       link_button("Youtube",
+                    "Videos largo en duracion ¡con el doble de diversion!",
+                    constants.YOUTUBE_URL),
