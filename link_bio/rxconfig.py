f62378074dc2e2daf7166d0a91b93013dd5806da
Littleroky <cantillocristianjose@gmail.com>
2026-08-21 20:41:21 -0500

    Api de la web

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -2,8 +2,13 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    cors_allowed_origins=[
+        "https://link-bio-navy-ring.reflex.run",
+        "http://localhost:3000"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
-    ]
+    ],
+    api_url="https://theobsidianboy-web.up.railway.app"
 )
