854bff0f575f34bf1e92c95ce21f69386454ed88
Littleroky <cantillocristianjose@gmail.com>
2026-08-24 19:31:38 -0500

    api twitch

--- link_bio/api/TwitchAPI.py
+++ link_bio/api/TwitchAPI.py
@@ -2,13 +2,17 @@ import os
 import dotenv 
 import requests
 import time
+from pathlib import Path
 
 class TwitchAPI:
+    env_path = Path(__file__).resolve().parent.parent / '.env'
+    dotenv.load_dotenv(dotenv_path=env_path)
 
     dotenv.load_dotenv()
 
     CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID")
     CLIENT_SECRET = os.environ.get("TWITCH_CLIENT_SECRET")
+
 
     def __init__(self):
         self.token = None
