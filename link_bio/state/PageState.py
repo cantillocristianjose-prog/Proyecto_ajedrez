bee2d9b6993f36faff2f4a2bc9d4e1e150379bbf
Littleroky <cantillocristianjose@gmail.com>
2026-09-01 16:58:43 -0500

    pruebs

--- link_bio/state/PageState.py
+++ link_bio/state/PageState.py
@@ -11,7 +11,7 @@ class PageState(rx.State):
     featured_info: list[Featured]
 
     async def check_live(self):
-        self.live_status = await live("theobsidianboy")
+        self.live_status = await live("rubencillo_88")
         if not self.live_status.live:
             self.next_live = utils.next_date(await schedule())
 
